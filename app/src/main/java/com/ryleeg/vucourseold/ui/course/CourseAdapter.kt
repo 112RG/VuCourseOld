a1eb45d12a18cb8913815a1557433f05b13e4457
Rylee George <ryleegeorge1@gmail.com>
2023-10-20 20:26:13 +1100

    Add search

--- app/src/main/java/com/ryleeg/vucourseold/ui/course/CourseAdapter.kt
+++ app/src/main/java/com/ryleeg/vucourseold/ui/course/CourseAdapter.kt
@@ -1,19 +1,24 @@
 package com.ryleeg.vucourseold.ui.course
 
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ryleeg.vucourseold.R
 import com.ryleeg.vucourseold.data.model.Course
+import java.util.Locale
 
-class CourseAdapter(private val data: List<Course>) :
-    RecyclerView.Adapter<CourseAdapter.MyViewHolder>() {
+class CourseAdapter(private var dataList: List<Course>) :
+    RecyclerView.Adapter<CourseAdapter.MyViewHolder>(), Filterable {
+    var dataFiltered: List<Course> = ArrayList()
 
-    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    class MyViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(property: Course) {
             val title = view.findViewById<TextView>(R.id.tvTitle)
@@ -28,18 +33,56 @@ class CourseAdapter(private val data: List<Course>) :
         }
     }
 
+    fun addData(list: List<Course>) {
+        dataList = list as ArrayList<Course>
+        dataFiltered = dataList
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return MyViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return dataFiltered.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(data[position])
+        holder.bind(dataFiltered[position])
     }
 
+    override fun getFilter(): Filter {
+        return object : Filter() {
 
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charString = constraint?.toString()?.lowercase(Locale.getDefault()) ?: ""
+                if (charString.isEmpty()) dataFiltered = dataList else {
+                    val filteredList = ArrayList<Course>()
+                    dataList
+                        .filter {
+                            (it.description.lowercase(Locale.getDefault()).contains(charString)) or
+                                    (it.name.lowercase(Locale.getDefault()).contains(charString))
+                        }
+                        .forEach { filteredList.add(it) }
+                    dataFiltered = filteredList
+                }
+                Log.e("performFiltering: t1: ", dataFiltered.size.toString())
+
+                return FilterResults().apply { values = dataFiltered }
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+
+                dataFiltered = if (results?.values == null)
+                    ArrayList()
+                else
+                    results.values as ArrayList<Course>
+                notifyDataSetChanged()
+
+                Log.e("performFiltering: t2 ", "called" + dataFiltered.size)
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/ryleeg/vucourseold/ui/course/CourseFragment.kt
+++ app/src/main/java/com/ryleeg/vucourseold/ui/course/CourseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.viewModelScope
@@ -28,19 +29,32 @@ class CourseFragment : Fragment() {
 
         _binding = FragmentCourseBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        
+
+
         val recyclerView = root.findViewById<RecyclerView>(R.id.recycler_view)
 
         recyclerView.layoutManager = LinearLayoutManager(this.context)
 
+
         // Initialize the adapter with an empty list
         val adapter = CourseAdapter(emptyList())
         recyclerView.adapter = adapter
+        root.findViewById<SearchView>(R.id.search_view)
+            .setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    adapter.filter.filter(query)
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.filter.filter(newText)
+                    return false
+                }
+            })
         viewModel.viewModelScope.launch {
             val courses = viewModel.getCourses()
-            recyclerView.adapter = CourseAdapter(
-                data = courses
-            )
+            adapter.addData(courses)
         }
         return root
     }
